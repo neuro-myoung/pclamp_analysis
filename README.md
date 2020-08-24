32eaa2344cd65e2a2f720bf6b1c2a63ee00fda53
twesleyb <tyler.w.bradshaw@duke.edu>
2020-08-24 12:59:40 -0400

    shreen shot of streamlit error

---
+++ README.md
@@ -0,0 +1,17 @@
+# pclamp-analysis
+
+## Usage
+Insure you have installed streamlit and the depenencies in `requirements.txt`.
+Insure you have set your PORT environmental variable to XXX.
+
+```bash
+# install requirements
+pip install -r requirements.txt
+
+# configure streamlit
+./setup.sh
+
+# launch the streamlit GUI:
+streamlit run pclamp_analysis.py
+```
+
--- pclamp_analysis.py
+++ pclamp_analysis.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import streamlit as st
 import re
 import numpy as np
@@ -9,7 +11,7 @@ import plotly.graph_objs as go
 import plotly.express as px
 from scipy.optimize import curve_fit
 import base64
-
+import sys
 
 ############################################## Define Functions ####################################################
 @st.cache(allow_output_mutation=True)
